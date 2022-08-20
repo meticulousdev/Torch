05e2e044fdbe8614a3e39c8882f8ebf029eb3317
meticulousdev <meticulousdev@gmail.com>
2022-08-20 20:20:27 +0900

    PyTorch GPU test

--- pytorch_gpu_test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-print(f"mps.is_built(): {torch.backends.mps.is_built()}")
-print(f"mps.is_available(): {torch.backends.mps.is_available()}")
-
-# mps.is_built(): True
-# mps.is_available(): True
