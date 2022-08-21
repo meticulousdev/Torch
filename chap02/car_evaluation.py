7a4adc7cb8b6feb9433c67b8d600b046e2f7b0a2
meticulousdev <meticulousdev@gmail.com>
2022-08-21 13:49:14 +0900

    TODO update

--- chap02/car_evaluation.py
+++ chap02/car_evaluation.py
@@ -84,7 +84,7 @@ categorical_embedding_sizes = [(col_size, min(50, (col_size + 1) // 2)) for col_
 
 # 임베딩 크기에 대한 정확한 규칙은 없지만, 칼럼의 고유 값 수를 2로 나누는 것을 많이 사용합니다.
 # (모든 범주형 칼럼의 고유 값 수, 차원의 크기)
-# (???) 임베딩과 차원의 크기
+# TODO 임베딩과 차원의 크기
 print(categorical_embedding_sizes)
 
 # %%
@@ -108,7 +108,8 @@ class Model(nn.Module):
         super().__init__()
         self.all_embeddings = nn.ModuleList([nn.Embedding(ni, nf) for ni, nf in embedding_size])
         self.embedding_dropout = nn.Dropout(p)
-        
+
+        # TODO layer 만드는 방법
         all_layers = []
         num_categorical_cols = sum((nf for ni, nf in embedding_size))
         input_size = num_categorical_cols 
