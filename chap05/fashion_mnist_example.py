eff1b977407668bed5af444a7fa65879936f70b5
meticulousdev <meticulousdev@gmail.com>
2022-08-28 00:36:08 +0900

    copy and paste

--- chap05/fashion_mnist_example.py
+++ chap05/fashion_mnist_example.py
@@ -48,3 +48,159 @@ for i in range(1, columns*rows +1):
     plt.imshow(img, cmap='gray')
 
 plt.show()
+
+# %%
+class FashionDNN(nn.Module):
+    def __init__(self):
+        super(FashionDNN,self).__init__()
+        self.fc1 = nn.Linear(in_features=784,out_features=256)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=256,out_features=128)
+        self.fc3 = nn.Linear(in_features=128,out_features=10)
+
+    def forward(self,input_data):
+        out = input_data.view(-1, 784)
+        out = F.relu(self.fc1(out))
+        out = self.drop(out)
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+# %%
+learning_rate = 0.001
+model = FashionDNN()
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print(model)
+
+# %%
+num_epochs = 5
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+
+predictions_list = []
+labels_list = []
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+    
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+        
+        outputs = model(train)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        count += 1
+    
+        if not (count % 50):    
+            total = 0
+            correct = 0        
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)            
+                test = Variable(images.view(100, 1, 28, 28))            
+                outputs = model(test)            
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()            
+                total += len(labels)
+            
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+        
+        if not (count % 500):
+            print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
+
+# %%
+class FashionCNN(nn.Module):    
+    def __init__(self):
+        super(FashionCNN, self).__init__()        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )       
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )        
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)       
+        return out
+
+# %%
+learning_rate = 0.001
+model = FashionCNN()
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print(model)
+
+# %%
+num_epochs = 5
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+
+predictions_list = []
+labels_list = []
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+    
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+        
+        outputs = model(train)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        count += 1
+    
+        if not (count % 50):    
+            total = 0
+            correct = 0        
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)            
+                test = Variable(images.view(100, 1, 28, 28))            
+                outputs = model(test)            
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()            
+                total += len(labels)
+            
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+        
+        if not (count % 500):
+            print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
